5c0b8432e10d63ba552770c2e2ac8a2cb455f16b
Vasim <vasim_shaikh777@yahoo.in>
2025-12-17 18:48:48 +0530

    feat: scaffold services (Auth, Users, Payments, Media, Chat) with helm charts

---
+++ services/media-service/src/main/java/com/bechde/media/MediaApplication.java
@@ -0,0 +1,5 @@
+package com.bechde.media;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MediaApplication { public static void main(String[] a){ SpringApplication.run(MediaApplication.class,a);} }
---
+++ services/users-service/src/main/java/com/bechde/users/UsersApplication.java
@@ -0,0 +1,5 @@
+package com.bechde.users;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class UsersApplication { public static void main(String[] a){ SpringApplication.run(UsersApplication.class,a);} }
